97964c9ddd9e37dd41b021b262c2565a0d100027
Sabbir Ahmed <bsse1530@iit.du.ac.bd>
2024-12-05 10:39:45 +0600

    Update weather.c

--- weather.c
+++ weather.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
-// Function to calculate linear regression parameters
 void linearRegression(double x[], double y[], int n, double *slope, double *intercept) {
     double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0;
 
-    // Calculate sums
     for (int i = 0; i < n; i++) {
         sumX += x[i];
         sumY += y[i];
@@ -12,7 +10,6 @@ void linearRegression(double x[], double y[], int n, double *slope, double *inte
         sumX2 += x[i] * x[i];
     }
 
-    // Calculate slope (m) and intercept (b)
     *slope = (n * sumXY - sumX * sumY) / (n * sumX2 - sumX * sumX);
     *intercept = (sumY - (*slope) * sumX) / n;
 }
@@ -27,7 +24,6 @@ int main() {
     double x[100], y[100]; // Arrays to store x and y values
     int n = 0;
 
-    // Read data from file
     while (fscanf(file, "%lf %lf", &x[n], &y[n]) == 2) {
         n++;
     }
